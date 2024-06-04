0106c74c022544e9f5fbb4f424125889ece64f0d
Ruan Pablo <rp824194@gmail.com>
2024-06-04 11:26:05 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -1,4 +1,4 @@
-const notas=[]
+/*const notas=[]
 
 notas.push(3)
 notas.push(7)
@@ -6,5 +6,9 @@ notas.push(4)
 notas.push(8)
 
 const soma = notas[0]+notas[1]+notas[2]+notas[3]
+console.log(soma/notas.length)*/
 
-console.log(soma/notas.length)
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+}
+
