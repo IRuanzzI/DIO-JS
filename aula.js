7f37a55fbc00845d867d3d4aa24223d6454c1dcb
Ruan Pablo <rp824194@gmail.com>
2024-06-04 11:41:59 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -8,7 +8,13 @@ notas.push(8)
 const soma = notas[0]+notas[1]+notas[2]+notas[3]
 console.log(soma/notas.length)*/
 
-for (let i = 1; i <= 100; i++) {
+/*for (let i = 1; i <= 100; i++) {
     console.log(i)
-}
+}*/
 
+const nome = 'Ruan Pablo Carvalho de Oliveira'
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i])
+    
+}
